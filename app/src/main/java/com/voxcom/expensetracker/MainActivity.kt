2fd6b24bf829b52c69f35b0cc94685323c41b15f
midhun1618 <midhumidhun342@gmail.com>
2025-05-18 11:00:16 +0530

    added dialog button

--- app/src/main/java/com/voxcom/expensetracker/MainActivity.kt
+++ app/src/main/java/com/voxcom/expensetracker/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.voxcom.expensetracker
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var input_amount : EditText
+    lateinit var addButton : Button
+    lateinit var submitButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        addButton.setOnClickListener {
+            val dialogView = layoutInflater.inflate(R.layout.addpopup, null)
+            input_amount = dialogView.findViewById(R.id.input_value)
+            submitButton = dialogView.findViewById(R.id.addThis)
+
+            val dialog = AlertDialog.Builder(this)
+                .setView(dialogView)
+                .create()
+
+            submitButton.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
         }
     }
 }
